0e21861a717ae3173a3265f8b941d8cf38751e5e
zhoufang <zhicheng.zhou.pro@gmail.com>
2020-06-28 14:46:56 +0200

    good

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Java 8 新特性
 
+![Alt text](photos/1.png?raw=true "Title")
+
+![Alt text](photos/2.png?raw=true "Title")
+## lambda 表达式
+```java
+       Runnable r2 = () -> System.out.println("i m r2");
+
+       r2.run();
+
+        System.out.println("*************");
+
+        Comparator<Integer> comparator = Integer::compareTo;
+        comparator.compare(14,13);
+```
+$ 语法
+* -> 这个是lambda操作符
+* 操作符的左边是行参列表 其实就是借口中抽象方法的形参列表
+* 右边是lambda体 其实就是重写抽象方法的方法体
+
+$ 几种使用场景
+* lambda 表达式的本质还是作为接口的实例
+* 无参数 无返回值
+* 数据类型可以省略 编译器可以自行推断得出 （类型推断）
+* 当lambda体只有一条语句 return 与大括号都可以省略
+
+## functional 函数式接口
--- src/test/java/com/zhicheng/service/UserServiceTest.java
+++ src/test/java/com/zhicheng/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Comparator;
 
 
 @RunWith(SpringRunner.class)
@@ -22,4 +23,26 @@ public class UserServiceTest {
        Assertions.assertThat(value).isEqualTo("qsds");
     }
 
+
+    @Test
+    public void lambdaExpressTest2() throws Exception {
+       Runnable r1 =  new Runnable() {
+           @Override
+           public void run() {
+               System.out.println("i m r1");
+           }
+       };
+
+       r1.run();
+
+
+       Runnable r2 = () -> System.out.println("i m r2");
+
+       r2.run();
+
+        System.out.println("*************");
+
+        Comparator<Integer> comparator = Integer::compareTo;
+        comparator.compare(14,13);
+    }
 }
